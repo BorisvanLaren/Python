44fc4c63dc80150c3f5363d76b862f3fc76f201a
BorisvanLaren <126478409+BorisvanLaren@users.noreply.github.com>
2023-03-28 00:18:37 +0200

    Add files via upload

---
+++ 5_3.py
@@ -0,0 +1,11 @@
+for q in range (1):
+    print(f"q: {q + 1},")
+for i in range (1):
+    print(f"q: {q + 1}, i: {i + 2}")
+for w in range (1):
+    print(f"q: {q + 1}, i: {i + 2}, w: {w + 3},")
+for y in range (1):
+    print(f"q: {q + 1}, i: {i + 2}, w: {w + 3}, y: {y + 4}")
+for s in range (1):
+    print(f"q: {q + 1}, i: {i + 2}, w: {w + 3}, y: {y + 4}, s: {s + 5}," )
+print ("have no idea how to get rid of those letters")
